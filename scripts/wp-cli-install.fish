1c94d33f64b81a849370ec4b100f2275409bc07d
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2025-06-19 06:34:37 +0530

    Use systemctl timer replacing cron for auto-updates

--- scripts/wp-cli-install.fish
+++ scripts/wp-cli-install.fish
@@ -60,15 +60,31 @@ wget -q -P ~/.config/systemd/user   https://github.com/pothi/wp-box/raw/refs/hea
 wget -q -P ~/.config/systemd/user   https://github.com/pothi/wp-box/raw/refs/heads/main/files/wpcli-update.timer
 
 systemctl --user enable wpcli-update.timer
+# sudo systemctl -M $USER@ --user enable wpcli-update.timer
 
 # if the following error is received...
 # Failed to connect to bus: $DBUS_SESSION_BUS_ADDRESS and $XDG_RUNTIME_DIR not defined (consider using --machine=<user>@.host --user to connect to bus of other user)
 # you are trying to do the above as sudo (after logging as normal user then root). Login as normal user and then execute the above.
+# you may use the alternative "sudo" method to make things work.
 
 systemctl --user start wpcli-update.timer
 systemctl --user enable wpcli-update.service
 systemctl --user start wpcli-update.service
+# sudo systemctl -M $USER@ --user start wpcli-update.timer
+# sudo systemctl -M $USER@ --user enable wpcli-update.service
+# sudo systemctl -M $USER@ --user start wpcli-update.service
 
 # Verify
 systemctl --user list-timers
+# sudo systemctl -M $USER@ --user list-timers
 
+exit
+
+### Old method to auto-update via cron
+crontab -l 2>/dev/null | grep -qF 'wp cli update'
+if test $status -ne 0
+    begin; crontab -l 2>/dev/null; echo; echo "@daily wp cli update --yes >> ~/log/wp-cli.log 2>&1"; end | crontab -
+    echo 'A new cron entry is created to update daily, if an update is available.'
+else
+    echo A cron entry is already in place to update wp-cli!
+end
