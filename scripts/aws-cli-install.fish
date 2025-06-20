7730025ece167ebc3dfe44383fc6e75d10aace3d
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2025-06-20 08:56:00 +0530

    Remove unused code

--- scripts/aws-cli-install.fish
+++ scripts/aws-cli-install.fish
@@ -65,25 +65,3 @@ else
     echo PATH is already present in cron.
 end
 
-exit
-
-# fish cli fish completion
-# TODO
-if not test -s "$fish_completion_dir/wp.fish"
-    if ! curl -LSs -o "$fish_completion_dir/wp.fish" https://github.com/wp-cli/wp-cli/raw/refs/heads/main/utils/wp.fish
-        echo >&2 'wp-cli: error downloading fish completion script.'
-    end
-end
-
-# set -l fish_trace non_empty_value
-# TODO
-#--- cron: auto-update aws cli ---#
-crontab -l 2>/dev/null | grep -qF 'wp cli update'
-if test $status -ne 0
-    begin; crontab -l 2>/dev/null; echo; echo "@daily wp cli update --yes >> ~/log/wp-cli.log 2>&1"; end | crontab -
-    echo 'A new cron entry is created to update daily, if an update is available.'
-else
-    echo A cron entry is already in place to update wp-cli!
-end
-
-# set -l fish_trace
