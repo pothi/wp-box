ef4a7e565e9144eff0b980f27d1c14d10e34f675
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2026-03-26 14:25:24 +0530

    Early checking of unzip command

--- scripts/aws-cli-install.fish
+++ scripts/aws-cli-install.fish
@@ -2,6 +2,10 @@
 
 set --local ver 2.0
 
+# TODO: uninstall script via argparse
+# TODO: Remove all except current version to save space.
+# TODO: Add update script via Cron
+
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 
 set -l debug
@@ -16,6 +20,8 @@ if not type -q check_status
     end
 end
 
+command -q unzip; or check_status $status 'Unzip is not installed.'
+
 # attempt to create BinDir and fish_completion_dir
 test -d $BinDir; or mkdir -p $BinDir
 check_status $status "Could not create $BinDir folder"
@@ -25,9 +31,8 @@ check_status $status "Could not create $fish_completion_dir folder"
 # add PATH
 fish_add_path $BinDir
 
-set -l tmpfile (mktemp)
-rm $tmpfile
 set -l tmpdir (mktemp -d)
+set -l tmpzipfile $tmpdir.zip
 set -l aws_cli "$BinDir/aws"
 
 set __os
@@ -57,15 +62,16 @@ switch $arch
 end
 test -z $debug; or echo "Arch: $__arch"
 
-test -z $debug; echo Temp File: $tmpfile
-test -z $debug; echo Temp Dir: $tmpdir
+test -z $debug; or echo Temp File: $tmpzipfile
+test -z $debug; or echo Temp Dir: $tmpdir
 
 printf '%-72s' "Downloading AWS CLI..."
-curl -LSs -o $tmpfile.zip https://awscli.amazonaws.com/awscli-exe-linux-$__arch.zip
+curl -LSs -o $tmpzipfile https://awscli.amazonaws.com/awscli-exe-linux-$__arch.zip
 check_status $status 'Could not download aws cli'
 
-command -q unzip; or check_status $status 'Unzip is not installed.'
-unzip -qq -d $tmpdir $tmpfile.zip
+unzip -tqq $tmpzipfile; or check_status $status 'Invalid zip file'
+
+unzip -qq -d $tmpdir $tmpzipfile
 
 if not test -s "$aws_cli"
     # Install aws cli
@@ -73,11 +79,17 @@ if not test -s "$aws_cli"
         chmod +x "$aws_cli"
     else
         echo >&2 Error installing aws cli.
+        rm $tmpzipfile
+        rm -rf $tmpdir
+        exit 1
     end
 else
     # Update aws cli, if existing installation is found
     if not $tmpdir/aws/install --install-dir $InstallDir --bin-dir $BinDir --update > /dev/null
         echo >&2 Error updating aws cli.
+        rm $tmpzipfile
+        rm -rf $tmpdir
+        exit 1
     end
 end
 
@@ -86,7 +98,7 @@ echo done.; echo
 # check the installation
 aws --version; or check_status $status 'aws-cli installation failed.'
 
-rm $tmpfile.zip
+rm $tmpzipfile
 rm -rf $tmpdir
 
 # include PATH into cron, if PATH doesn't exist
