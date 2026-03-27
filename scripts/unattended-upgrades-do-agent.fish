f2dffa7e8d22f977cf0608c0fc014b8f70e175cd
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2026-03-27 16:06:38 +0530

    Add unattended upgrade for DigitalOcean Agent

---
+++ scripts/unattended-upgrades-do-agent.fish
@@ -0,0 +1,49 @@
+#!usr/bin/env fish
+
+set package_name do-agent
+set config_file /etc/apt/apt.conf.d/51unattended-upgrades-$package_name
+set apt_origin (apt-cache policy | grep -A2 $package_name | grep release | sed 's/release//' | awk -F, '{print $1}' | awk -F= '{print $2}')
+
+echo "Origin: $apt_origin"
+
+set unattended_policy "Unattended-Upgrade::Origins-Pattern { \"origin=$apt_origin\" };"
+echo Policy: $unattended_policy
+echo
+
+# Create the unattended-upgrade config file.
+echo $unattended_policy > $config_file
+
+echo Contents of $config_file
+cat /etc/apt/apt.conf.d/51unattended-upgrades-$package_name
+echo
+
+echo Unattended upgrade is configured for $package_name
+
+echo If something is amiss, remove $config_file
+echo
+
+set disable_warning /etc/apt/apt.conf.d/90disablescriptwarning
+echo Creating $disable_warning to disable warning message/s from apt.
+test -f $disable_warning; or echo "Apt::Cmd::Disable-Script-Warning \"true\";" > $disable_warning
+echo
+
+echo Running tests...
+
+# check if the package is available for upgrade
+echo Checking if $package_name has a pending upgrade...
+apt list --installed | grep -q $package_name
+if test $status -eq 0
+    echo $package_name has a pending upgrade.
+
+    echo Dry-running unattended-upgrade...
+    unattended-upgrade --dry-run 2>&1 | grep -q $package_name
+    if test $status -eq 0
+        echo Dry-run successfully upgraded $package_name.
+    else
+        echo Dry-run did not upgrade $package_name
+        echo Check if anything went wrong above.
+    end
+else
+    echo $package_name does not have any pending upgrade.
+end
+
