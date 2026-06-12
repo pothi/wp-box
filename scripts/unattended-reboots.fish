9e8d16cc1e8a0409641f53b1f64ad0aa44bd9a18
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2026-06-12 07:21:21 +0530

    Improve docs

--- scripts/unattended-reboots.fish
+++ scripts/unattended-reboots.fish
@@ -1,40 +1,42 @@
 #!/usr/bin/env fish
 
-set ver 1.0
+set ver 1.1
 
 # Careful when rebooting a server, unattended!
 
-# programming env: these switches turn some bugs into errors
-# set -o errexit -o pipefail -o noclobber -o nounset
-
-# what's done here
+# TODO
+#   + Argument to supply reboot time
+#   + Option to self-update
+#   + Option to disable unattended reboots (by removing the file)
 
 # variables
 set REBOOT_TIME "03:45"
+set conf_file /etc/apt/apt.conf.d/52unattended-reboot
 
-# we used ":" in sed in unattended-upgrades.sh file.
-# here, since reboot time has ":" in it, we can't use ":" in sed as separator.
-printf '%-72s' "Setting up unattended reboots..."
+printf '%-66s' "Setting up unattended reboots..."
 
-set apt_file /etc/apt/apt.conf.d/52unattended-reboot
-echo > $apt_file
+echo > $conf_file
 
-echo '// Automatically reboot *WITHOUT CONFIRMATION* if' >> $apt_file
-echo '//  the file /var/run/reboot-required is found after the upgrade' >> $apt_file
-echo 'Unattended-Upgrade::Automatic-Reboot "true";' >> $apt_file
-echo >> $apt_file
+echo '// Automatically reboot *WITHOUT CONFIRMATION* if' >> $conf_file
+echo '//  the file /var/run/reboot-required is found after the upgrade' >> $conf_file
+echo 'Unattended-Upgrade::Automatic-Reboot "true";' >> $conf_file
+echo >> $conf_file
 
-echo '// Automatically reboot even if there are users currently logged in' >> $apt_file
-echo '// when Unattended-Upgrade::Automatic-Reboot is set to true' >> $apt_file
-echo 'Unattended-Upgrade::Automatic-Reboot-WithUsers "false";' >> $apt_file
-echo >> $apt_file
+echo '// Automatically reboot even if there are users currently logged in' >> $conf_file
+echo '// when Unattended-Upgrade::Automatic-Reboot is set to true' >> $conf_file
+echo 'Unattended-Upgrade::Automatic-Reboot-WithUsers "false";' >> $conf_file
+echo >> $conf_file
 
-echo '// If automatic reboot is enabled and needed, reboot at the specific' >> $apt_file
-echo '// time instead of immediately' >> $apt_file
-echo '//  Default: "now"' >> $apt_file
-echo "Unattended-Upgrade::Automatic-Reboot-Time \"$REBOOT_TIME\";" >> $apt_file
-echo >> $apt_file
+echo '// If automatic reboot is enabled and needed, reboot at the specific' >> $conf_file
+echo '// time instead of immediately' >> $conf_file
+echo '//  Default: "now"' >> $conf_file
+echo "Unattended-Upgrade::Automatic-Reboot-Time \"$REBOOT_TIME\";" >> $conf_file
+echo >> $conf_file
 
 echo done.
 
+echo "Reboot Time: $REBOOT_TIME"
+# echo To disable unattended reboots, remove the file $conf_file
 
+echo The contents of $conf_file ...
+cat $conf_file
